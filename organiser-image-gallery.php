eb9dce23cc6766a79eea1aff90ca00c4ca5d69ea
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-16 23:43:13 +0000

    Update organiser-image-gallery.php

--- organiser-image-gallery.php
+++ organiser-image-gallery.php
@@ -694,7 +694,7 @@ function create_tec_organizer_category_with_images($category_name, $image_urls, 
 
 #drop-zone:hover {
     border-color: #0AC6A6!important;
-    background-color:#0AC6A6!important;
+    background-color:#E6F9F6!important;
 }
 
 #drop-zone:hover .primary-icon {
