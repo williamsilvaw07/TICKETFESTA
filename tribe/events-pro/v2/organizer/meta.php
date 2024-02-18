b0028dd6ab6685962ec8f460e44da73bede1fe8d
Arnab Mondal <armondal.am@gmail.com>
2024-02-18 16:49:53 +0600

    my accounr

--- tribe/events-pro/v2/organizer/meta.php
+++ tribe/events-pro/v2/organizer/meta.php
@@ -124,7 +124,7 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
             echo "Following " . $_POST['follow'];
           } else {
 
-          echo  wp_login_url();
+          echo  get_permalink( get_option('woocommerce_myaccount_page_id') );
         }
     } 
 }
