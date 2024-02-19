7f90adcbdb2ec291a9fc0c62051d06cfe955c5c7
Arnab Mondal <armondal.am@gmail.com>
2024-02-19 23:10:31 +0600

    remove inline code

--- functions.php
+++ functions.php
@@ -2656,34 +2656,3 @@ function save_subscribed_organiser_checkbox( $order_id ) {
     }
 }
 
-function ticketfeasta_inline_js(){
-    ?>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-
-            var termsWrapper = document.querySelector('.woocommerce-terms-and-conditions-wrapper');
-
-            if (termsWrapper) {
-                // Create a checkbox input field
-                var checkbox = document.createElement('input');
-                checkbox.type = 'checkbox';
-                checkbox.name = 'subscribed_organiser';
-                checkbox.id = 'subscribed_organiser';
-                checkbox.value = 'checked'; 
-
-                checkbox.checked = true;
-                var label = document.createElement('label');
-                label.htmlFor = 'subscribed_organiser';
-                label.appendChild(document.createTextNode('Subscribe to event organizer.'));
-
-                // Append the checkbox and label to the terms wrapper
-                termsWrapper.appendChild(checkbox);
-                termsWrapper.appendChild(label);
-            }
-        });
-
-    </script>
-<?php
-
-}
-add_action('wp_footer', 'ticketfeasta_inline_js');
