dbaec8f5c45c38a429a7fa98f97f057e7d93a14d
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-22 21:23:14 +0000

    Update meta.php

--- tribe/events-pro/v2/organizer/meta.php
+++ tribe/events-pro/v2/organizer/meta.php
@@ -959,6 +959,10 @@ jQuery(document).ready(function($) {
 
 
 <style>
+    .followers-count , .event-count{
+        font-size:18px!important;
+        font-weight:600
+    }
     .organizer_text_dec_info{
         display: flex;
     gap: 20px;
