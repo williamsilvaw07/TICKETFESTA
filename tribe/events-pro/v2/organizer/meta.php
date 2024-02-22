c0a0ea62006ca6b6fd7adbc949814816135eb96d
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-22 20:36:02 +0000

    Update meta.php

--- tribe/events-pro/v2/organizer/meta.php
+++ tribe/events-pro/v2/organizer/meta.php
@@ -58,7 +58,6 @@ $template_vars = array_merge( [ 'organizer' => $organizer, ], $conditionals )
 <div <?php tribe_classes( $classes ); ?>>
 
 
-    <div class="organizer_profile_main_div">
 
 <div class="organizer_profile_bk">
 
@@ -200,8 +199,8 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 </div>
 <!-- organizer name end -->
 </div>
-</div>
-</div>
+
+
 
 <!-- organizer profile nav -->
 <div class="organizer_navbar">
@@ -534,6 +533,7 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 
 
 	
+</div>
 
 
 
