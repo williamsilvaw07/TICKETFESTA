c04e103db366b2c53e60a6ebd12452caa5b42d0b
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-23 15:22:12 +0000

    Update single-event.php

--- tribe-events/single-event.php
+++ tribe-events/single-event.php
@@ -1107,9 +1107,7 @@ html body .single_event_sections {
         max-width: 700px!important;
     width: 100%!important;
     }
-    .tribe-events-event-meta_venue .tribe-events-meta-group-details , .tribe-events-meta-group-organizer , .tribe-events-event-meta , .tribe-events-meta-group-organizer{
-display:none!important
-    }
+  
 
 
 .single_event_sections h2{
