04be2066e4793a117999186555f13d0ab0ba3942
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-23 23:37:28 +0000

    Update custom-function-frontend.js

--- custom-function-frontend.js
+++ custom-function-frontend.js
@@ -1,20 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     var searchIcon = document.querySelector('.header_search_icon');
-    var searchPopup = document.getElementById('searchPopup');
 
-    if (!searchIcon || !searchPopup) {
-        console.error('Required elements not found.');
+    if (!searchIcon) {
+        console.error('Search icon not found.');
         return;
     }
 
     searchIcon.addEventListener('click', function() {
         console.log('Search icon clicked.');
-        searchPopup.style.display = 'block'; // Ensure the search popup is displayed
 
-        // Select all .tt-dataset elements within the searchPopup and set them to display: block
-        var datasets = searchPopup.querySelectorAll('.tt-dataset');
-        datasets.forEach(function(dataset) {
-            dataset.style.display = 'block';
-        });
+        // Find the .tt-menu element within #ecsa-search
+        var ttMenu = document.querySelector('#ecsa-search .tt-menu');
+
+        if (ttMenu) {
+            // Apply the CSS styles to the .tt-menu element
+            ttMenu.style.width = '100%';
+            ttMenu.style.display = 'inline-block';
+            ttMenu.style.maxHeight = '300px';
+            ttMenu.style.overflowX = 'hidden';
+            ttMenu.style.overflowY = 'auto';
+            ttMenu.style.marginTop = '5px';
+            ttMenu.style.padding = '0';
+            ttMenu.style.backgroundColor = '#fff';
+            ttMenu.style.border = '1px solid rgba(0,0,0,.2)';
+            ttMenu.style.borderRadius = '2px';
+            ttMenu.style.boxShadow = '0 2px 5px rgba(0,0,0,.15)';
+            ttMenu.style.position = 'relative!important';
+            ttMenu.style.top = '0!important';
+        } else {
+            console.error('.tt-menu element not found within #ecsa-search.');
+        }
     });
 });
