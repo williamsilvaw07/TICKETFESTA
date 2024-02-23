8dc2e8555224150db3c3cf3bfdaa4aedeb123b13
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-23 20:25:29 +0000

    Update custom-function-frontend.js

--- custom-function-frontend.js
+++ custom-function-frontend.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var searchIcon = document.querySelector('.header_search_icon');
     var searchPopup = document.getElementById('searchPopup');
-    var searchInput = document.getElementById('ecsa-search-box');
 
-    if (!searchIcon || !searchPopup || !searchInput) {
+    if (!searchIcon || !searchPopup) {
         console.error('Required elements not found.');
         return;
     }
@@ -11,24 +10,5 @@ document.addEventListener('DOMContentLoaded', function() {
     searchIcon.addEventListener('click', function() {
         console.log('Search icon clicked.');
         searchPopup.style.display = 'block'; // Ensure the search popup is displayed
-
-        // Function to simulate typing in the input field
-        function simulateTyping() {
-            console.log('Simulating typing in the input field.');
-            searchInput.focus(); // Focus the input field before simulating typing
-
-            // Simulate a keydown event for the character "t"
-            var event = new KeyboardEvent('keydown', {
-                key: 't',
-                code: 'KeyT',
-                which: 84,
-                keyCode: 84
-            });
-
-            searchInput.dispatchEvent(event);
-        }
-
-        // Start an interval to simulate typing every 1 second
-        setInterval(simulateTyping, 1000);
     });
 });
