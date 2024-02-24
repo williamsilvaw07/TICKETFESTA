cafb2644d94089fa8b1bac201784c1273645f156
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-24 00:19:25 +0000

    Update custom-function-frontend.js

--- custom-function-frontend.js
+++ custom-function-frontend.js
@@ -14,21 +14,23 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-
 document.addEventListener('DOMContentLoaded', function() {
     var searchIcon = document.querySelector('.header_search_icon'); // Selector for the search icon
 
     if (searchIcon) {
         searchIcon.addEventListener('click', function() {
-            var typeaheadContainer = document.querySelector('.twitter-typeahead');
-            var typeaheadInput = typeaheadContainer ? typeaheadContainer.querySelector('.typeahead.tt-input') : null;
+            // Delay the focus action by 1 second after the click
+            setTimeout(function() {
+                var typeaheadContainer = document.querySelector('.twitter-typeahead');
+                var typeaheadInput = typeaheadContainer ? typeaheadContainer.querySelector('.typeahead.tt-input') : null;
 
-            if (typeaheadInput && !typeaheadInput.disabled) {
-                typeaheadInput.focus(); // Focus on the input field to show the caret
-                console.log('Focused on typeahead input, caret should be visible.');
-            } else {
-                console.log('Typeahead input is not found or it is disabled.');
-            }
+                if (typeaheadInput && !typeaheadInput.disabled) {
+                    typeaheadInput.focus(); // Focus on the input field to show the caret
+                    console.log('Focused on typeahead input, caret should be visible.');
+                } else {
+                    console.log('Typeahead input is not found or it is disabled.');
+                }
+            }, 1000); // 1000 milliseconds = 1 second
         });
     } else {
         console.log('Search icon not found.');
