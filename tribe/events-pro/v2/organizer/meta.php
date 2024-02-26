88232ded1c9412401453d326b1a170e51884639d
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-26 17:41:36 +0000

    Update meta.php

--- tribe/events-pro/v2/organizer/meta.php
+++ tribe/events-pro/v2/organizer/meta.php
@@ -980,7 +980,7 @@ jQuery(document).ready(function($) {
 
 // Make the entire event card clickable without affecting interactive elements like buttons and links
 document.addEventListener('DOMContentLoaded', function() {
-    var articles = document.querySelectorAll('.event-image s');
+    var articles = document.querySelectorAll('.event-image a');
 
     articles.forEach(function(article) {
         // Add click event listener to each article
