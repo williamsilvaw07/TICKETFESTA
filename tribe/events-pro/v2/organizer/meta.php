d733c09c45ca530a41bb94702a53dc1ff58a5439
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-26 17:52:31 +0000

    Update meta.php

--- tribe/events-pro/v2/organizer/meta.php
+++ tribe/events-pro/v2/organizer/meta.php
@@ -993,7 +993,7 @@ jQuery(document).ready(function($) {
         } else {
             console.log('Image URL not found.'); // Log error message if image URL is not found
         }
-    }, 1000); // Delay of 30ms
+    }, 3000); // Delay of 30ms
 });
 
 
