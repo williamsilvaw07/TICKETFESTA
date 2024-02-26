b92c61af48e9d98dd610430ac7b847afaed154a9
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-26 17:33:35 +0000

    Update custom-function-frontend.js

--- custom-function-frontend.js
+++ custom-function-frontend.js
@@ -131,5 +131,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    var imageElement = document.querySelector('.home_bk_image_imga img');
+    var targetElement = document.querySelector('.tribe-events-view--photo');
 
+    if (imageElement && targetElement) {
+        var imageUrl = imageElement.getAttribute('src');
+        targetElement.style.backgroundImage = 'url("' + imageUrl + '")';
+        targetElement.classList.add('custom-background-class'); // Add this class if you want to apply additional styles
+    }
+});
 
