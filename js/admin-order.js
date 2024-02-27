635dd1e4751727ed459e1c9d73b09d975b9eec11
Arnab Mondal <armondal.am@gmail.com>
2024-02-27 21:57:58 +0600

    Update admin-order.js

--- js/admin-order.js
+++ js/admin-order.js
@@ -1,10 +1,25 @@
 jQuery(document).ready(function($) {
     var textContent = $('.tec-tickets__admin-orders-report-overview-ticket-type-list-item-ticket-name').text().trim();
-    var Ticketprice = parseFloat(textContent.match(/\d+(\.\d+)?/)[0]);
+    var ticketPrice = parseFloat(textContent.match(/\d+(\.\d+)?/)[0]);
     
     var textContent = $('.tec-tickets__admin-orders-report-overview-ticket-type-list-item-stat').text().trim();
     var quantity = parseFloat(textContent.match(/\b\d+\b/)[0]);
 
-    console.log('Ticketprice: ', Ticketprice);
+    console.log('Ticketprice: ', ticketPrice);
     console.log('Quantity: ', quantity);
+    var ticketFee = get_tribe_ticket_fee(ticketPrice, quantity = 1);
+    let newText = '<strong>Total Site Fees:</strong> ' + ticketFee;
+    jQuery('.tribe-event-meta-total-site-fees').text(newText);
 });
+
+function get_tribe_ticket_fee(ticketAmount, quantity = 1){
+    ticketSiteFee = 0;
+    if(ticketAmount < 50 ){
+        ticketSiteFee += (ticketAmount * .03 + 0.02) * quantity;
+    }
+    if(ticketAmount > 50 ){
+        ticketSiteFee += (ticketAmount *  .01 + 0.02) * quantity;
+    }
+
+    return ticketSiteFee.toFixed(2);
+}
