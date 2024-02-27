bcb013dac3054eccb0d3643d02845a645104872f
Arnab Mondal <armondal.am@gmail.com>
2024-02-27 21:50:37 +0600

    Update admin-order.js

--- js/admin-order.js
+++ js/admin-order.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($) {
-    console.log('hello from order js');
     var textContent = $('.tec-tickets__admin-orders-report-overview-ticket-type-list-item-ticket-name').text().trim();
-
-    // Extract the numeric value using regular expression
     var Ticketprice = parseFloat(textContent.match(/\d+(\.\d+)?/)[0]);
+    
+    var textContent = $('.tec-tickets__admin-orders-report-overview-ticket-type-list-item-stat').text().trim();
+    var quantity = parseFloat(textContent.match(/\b\d+\b/)[0]);
 
-    // Output the parsed float value
     console.log('Ticketprice: ', Ticketprice);
+    console.log('Quantity: ', quantity);
 });
