c9d12df2ee7618c0d6f0c1cf64622ab198225b65
Arnab Mondal <armondal.am@gmail.com>
2024-02-29 11:28:31 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -163,3 +163,120 @@ function change_my_account_orders_title($translated_text, $text, $domain)
 }
 add_filter('gettext', 'change_my_account_orders_title', 20, 3);
 
+
+///Redirect right to checkout page
+/*
+add_filter('woocommerce_add_to_cart_validation', 'custom_redirect_after_add_to_cart', 10, 3);
+
+function custom_redirect_after_add_to_cart($passed, $product_id, $quantity) {
+    if ($passed) {
+        wp_safe_redirect(wc_get_checkout_url());
+        exit;
+    }
+    return $passed;
+}
+*/
+
+
+// $custom_post = get_post(1742);
+// echo "<pre>";
+// $product = wc_get_product(1835);
+// var_dump($product);
+// echo "</pre>";
+
+// $userId = get_current_user_id();
+// var_dump($userId);
+// var_dump((int) $custom_post->post_author === $userId);
+// echo "</pre>";
+// die();
+
+function iam00_return_ticket_associate_with_event()
+{
+    //check nonce
+    if (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'add-coupon-nonce')) {
+        $response_data = array(
+            'message' => 'Invalid nonce',
+        );
+        wp_send_json_success($response_data, 422);
+        wp_die();
+    }
+    if (isset($_POST['event_id'])) {
+        $eventId = (int) $_POST['event_id'];
+        global $wpdb;
+        $results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}postmeta WHERE meta_key = '_tribe_wooticket_for_event' AND meta_value = {$eventId}");
+
+        $ids = [];
+
+        foreach ($results as $key => $value) {
+            $ids[] = $value->post_id;
+        }
+        $args = array(
+            'include' => $ids, // Replace with your actual product IDs
+            'return' => 'objects' // Ensure product objects are returned
+        );
+        $products = wc_get_products($args);
+
+        $response = [];
+
+        foreach ($products as $product) {
+            // Access product data:
+            $response[$product->get_id()] = $product->get_name();
+        }
+
+        wp_send_json_success($response, 200);
+        die();
+    } else {
+        $response_data = array(
+            'message' => 'event_id missing',
+        );
+        wp_send_json_success($response_data, 422);
+        die();
+    }
+}
+add_action('wp_ajax_get_event_ticket_action', 'iam00_return_ticket_associate_with_event'); // For logged-in users
+
+function dd($object){
+    echo "<pre>";
+    var_dump( $object);
+    echo "</pre>";
+    exit();
+}
+
+function iam00_get_coupon_associate_with_event($eventId){
+     // Set up query arguments
+     $args = array(
+        'post_type' => 'product',
+        'posts_per_page' => -1,
+        'meta_query' => array(
+            array(
+                'key' => '_tribe_wooticket_for_event',
+                'value' => $eventId,
+            ),
+        ),
+    );
+
+    // Query products
+    $query = new WP_Query($args);
+
+    // Get product IDs
+    $products = $query->posts;
+    $product_ids = [];
+    foreach ($products as $product) {
+        $product_ids[] = $product->ID;
+    }
+    
+    // Get all coupons
+    $args = array(
+        'post_type' => 'shop_coupon',
+        'posts_per_page' => -1,
+        'meta_query' => array(
+            array(
+                'key' => 'product_ids',
+                'value' => $product_ids,
+                'compare' => 'IN',
+            ),
+        ),
+    );
+    $query = new WP_Query($args);
+    return $query->posts;
+}
