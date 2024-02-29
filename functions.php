2f88d347b0a63c1b8429c7baabd960e5e96a6889
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-29 17:45:37 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -95,23 +95,6 @@ function woocommerce_cart_item_name_event_title( $title, $values, $cart_item_key
  
     return $title;
 }
-////CHECKOUT
-
-/**
- * Flux checkout - Allow custom CSS files.
- *
- * @param array $sources Sources.
- *
- * @return array
- */
-function flux_allow_custom_css_files( $sources ) {
-	$sources[] = 'http://site.com/wp-content/themes/storefront/style.css';
-	return $sources;
-}
-add_filter( 'flux_checkout_allowed_sources', 'flux_allow_custom_css_files' );
-
-add_action( 'flux_before_layout', 'get_header' );
-add_action( 'flux_after_layout', 'get_footer' );
 
 ////FONTASWER
 
@@ -122,6 +105,11 @@ function my_theme_enqueue_scripts() {
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
 
 
+
+function enqueue_font_awesome() {
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_font_awesome');
 
 ////END
 
