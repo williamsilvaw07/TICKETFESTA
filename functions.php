3fc760b59a6c2b41df65ed0996a0e0b1ad573f96
Arnab Mondal <armondal.am@gmail.com>
2024-02-29 11:29:30 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -280,3 +280,230 @@ function iam00_get_coupon_associate_with_event($eventId){
     $query = new WP_Query($args);
     return $query->posts;
 }
+
+function iam00_return_coupon_associate_with_event()
+{
+    //check nonce
+    if (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'add-coupon-nonce')) {
+        $response_data = array(
+            'message' => 'Invalid nonce',
+        );
+        wp_send_json_success($response_data, 422);
+        wp_die();
+    }
+    if (isset($_POST['event_id'])) {
+        $eventId = (int) $_POST['event_id'];
+        $coupons = iam00_get_coupon_associate_with_event($eventId);
+        // Loop through each coupon
+        foreach ($coupons as $coupon) {
+            
+            $coupon = new WC_Coupon($coupon->ID);
+            
+            $expire_date = $coupon->get_date_expires();
+            $formatted_expire_date = $expire_date ? date('Y-m-d H:i', strtotime($expire_date)) : '';
+            
+            $start_date = $coupon->get_date_created();
+            $formatted_start_date = $start_date ? date('Y-m-d H:i', strtotime($start_date)) : '';
+
+            $response_data = array(
+                'code' => $coupon->get_code(),
+                'discount_type' => $coupon->get_discount_type(),
+                'amount' => $coupon->get_amount(),
+                'individual_use' => $coupon->get_individual_use(),
+                'description' => $coupon->get_description(),
+                'usage_limit' => $coupon->get_usage_limit(),
+                'expire_date' => $formatted_expire_date,
+                'start_date' => $formatted_start_date,
+            );
+
+            // Output or use $response_data as needed
+            // print_r($response_data);
+
+            $response[] = $response_data;
+        }
+        wp_send_json_success($response, 200);
+        die();
+    } else {
+        $response_data = array(
+            'message' => 'event_id missing',
+        );
+        wp_send_json_success($response_data, 422);
+        die();
+    }
+}
+add_action('wp_ajax_get_event_ticket_coupon_action', 'iam00_return_coupon_associate_with_event'); // For logged-in users
+
+function iam00_create_woo_coupon_for_ticket()
+{
+    //check nonce
+    if (!isset($_POST['nonce'])) {
+        $response_data = array(
+            'message' => 'Nonce missing',
+        );
+        wp_send_json_success($response_data, 422);
+        wp_die();
+    } else {
+        if (!wp_verify_nonce($_POST['nonce'], 'add-coupon-nonce')) {
+            // Nonce is invalid, handle accordingly
+            $response_data = array(
+                'message' => 'Invalid nonce',
+            );
+            wp_send_json_success($response_data, 422);
+            wp_die();
+        }
+    }
+    // check woocommerce active
+    if (!is_plugin_active('woocommerce/woocommerce.php')) {
+        $response_data = array(
+            'message' => 'Woocommerce is not active',
+        );
+        wp_send_json_success($response_data, 422);
+        die();
+    }
+    //Get the user id
+    $userId = get_current_user_id();
+    //Get Event id && Check if this event own by this user
+    if (isset($_POST['event_id'])) {
+        $eventId = $_POST['event_id'];
+        $event = get_post($eventId);
+
+        if (!$event) {
+            $response_data = array(
+                'message' => 'event not found',
+            );
+            wp_send_json_success($response_data, 422);
+            die();
+        }
+        if ((int) $event->post_author !== $userId) {
+            $response_data = array(
+                'message' => 'event not belongs to you',
+            );
+            wp_send_json_success($response_data, 422);
+            die();
+        }
+    } else {
+        $response_data = array(
+            'message' => 'event_id missing',
+        );
+        wp_send_json_success($response_data, 422);
+        die();
+    }
+    //Get the products id
+    if (isset($_POST['product_ids']) && is_array($_POST['product_ids'])) {
+        $product_ids = array_map('intval', $_POST['product_ids']);
+        $invalid_product_id = false;
+        //Product Belongs to the Event
+        foreach ($product_ids as $product_id) {
+            $post_id = get_post_meta($product_id, '_tribe_wooticket_for_event', true);
+            if ($post_id !== $eventId) {
+                $invalid_product_id = true;
+            }
+        }
+        if ($invalid_product_id) {
+            $response_data = array(
+                'message' => 'Invalid product in product list',
+            );
+            wp_send_json_success($response_data, 422);
+            die();
+        }
+    } else {
+        $response_data = array(
+            'message' => 'product_ids is missing',
+        );
+        wp_send_json_success($response_data, 422);
+        die();
+    }
+    if (isset($_POST['coupon_code'])) {
+        $coupon_code = $_POST['coupon_code'];
+        $discount_type = isset($_POST['discount_type']) ? $_POST['discount_type'] : 'percent';
+        $amount = isset($_POST['amount']) ? $_POST['amount'] : 0;
+        $individual_use = isset($_POST['individual_use']) ? $_POST['individual_use'] : true;
+        $description = isset($_POST['description']) ? $_POST['description'] : '';
+        $usage_limit = isset($_POST['usage_limit']) ? $_POST['usage_limit'] : 1;
+
+        //Check if coupon name unique
+        if (wc_get_coupon_id_by_code($coupon_code)) {
+            $response_data = array(
+                'message' => 'Coupon already exists with the code: ' . $coupon_code,
+            );
+            wp_send_json_success($response_data, 422);
+            die();
+        }
+        //Create coupon associate with the product id
+        $coupon = new WC_Coupon();
+        // Set coupon properties
+        $coupon->set_code($coupon_code);
+        $coupon->set_product_ids($product_ids);
+        $coupon->set_discount_type($discount_type); // Change this to 'fixed_cart' for a fixed amount discount
+        $coupon->set_amount($amount); // Change this to the discount amount (either percentage or fixed amount)
+        $coupon->set_individual_use($individual_use); // Set to true if the coupon should be used alone
+        $coupon->set_description($description);
+        $coupon->set_usage_limit($usage_limit); // Change this to the maximum number of times the coupon can be used
+
+        $expire_date = '';
+        if (isset($_POST['end_date_time']) && $_POST['end_date_time'] != '' ) {
+            $expire_date = strtotime($_POST['end_date_time']);
+            $coupon->set_date_expires($expire_date);
+        }
+
+        $start_date = '';
+        if (isset($_POST['start_date_time'])) {
+            $start_date = strtotime($_POST['start_date_time']);
+            $coupon->set_date_created($start_date);
+        }
+
+        // Save the coupon
+        $coupon->save();
+
+        
+        $formatted_expire_date = $expire_date ? date('Y-m-d H:i', strtotime($expire_date)) : '';
+        $formatted_start_date = $expire_date ? date('Y-m-d H:i', strtotime($expire_date)) : '';
+
+
+        $response_data = array(
+            'message' => 'Coupon created successfully',
+            'code' => $coupon_code,
+            'discount_type' => $discount_type,
+            'amount' => $amount,
+            'individual_use' => $individual_use,
+            'description' => $description,
+            'usage_limit' => $usage_limit,
+            'expire_date' => $formatted_expire_date,
+            'start_date' => $formatted_start_date,
+            'usage_limit' => $usage_limit,
+        );
+        wp_send_json_success($response_data, 200);
+        die();
+
+        //return coupon details
+
+    } else {
+        $response_data = array(
+            'message' => 'coupon_code is missing',
+        );
+        wp_send_json_success($response_data, 422);
+        die();
+    }
+    // Always use die() at the end of your handler function
+    die();
+}
+add_action('wp_ajax_add_coupon_action', 'iam00_create_woo_coupon_for_ticket'); // For logged-in users
+
+function iam00_create_event()
+{
+    $event = tribe_events()
+        ->set_args([
+            'title' => 'My Event',
+            'start_date' => '+2 days 15:00:00',
+            'duration' => HOUR_IN_SECONDS,
+            'status' => 'publish',
+        ])
+        ->create();
+
+    $response_data = array(
+        'message' => 'Event Created',
+    );
+    wp_send_json_success($response_data, 200);
+    wp_die();
+}
+add_action('wp_ajax_add_event', 'iam00_create_event');
