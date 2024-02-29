26eab43df23f86e30e99685b7eb5fe245b777711
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-29 02:10:32 +0000

    Update meta.php

--- tribe/events-pro/v2/organizer/meta.php
+++ tribe/events-pro/v2/organizer/meta.php
@@ -262,19 +262,19 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 
 <!-- organizer profile nav -->
 <div class="organizer_navbar">
-	<div class="organizer_nav-item organizer_events">
-	  <img src="https://thaynna-william.co.uk/wp-content/uploads/2023/12/calendar-1.png" alt="Events Icon" class="organizer_nav-icon" width="21">
-	  <span>Events</span>
-	</div>
-	<div class="organizer_nav-item organizer_Gallery">
-	  <img src="https://thaynna-william.co.uk/wp-content/uploads/2023/12/image-gallery-1.png" alt="Gallery Icon" class="organizer_nav-icon" width="21">
-	  <span>Gallery</span>
-	</div>
-	<div class="organizer_nav-item organizer_about">
-	  <img src="https://thaynna-william.co.uk/wp-content/uploads/2023/12/user-1-1.png" alt="About Icon" class="organizer_nav-icon profile_icon_nav" width="21">
-	  <span>About</span>
-	</div>
+  <div class="organizer_nav-item organizer_events">
+    <i class="fa fa-calendar-alt organizer_nav-icon" aria-hidden="true"></i>
+    <span>Events</span>
   </div>
+  <div class="organizer_nav-item organizer_Gallery">
+    <i class="fa fa-images organizer_nav-icon" aria-hidden="true"></i>
+    <span>Gallery</span>
+  </div>
+  <div class="organizer_nav-item organizer_about">
+    <i class="fa fa-user organizer_nav-icon profile_icon_nav" aria-hidden="true"></i>
+    <span>About</span>
+  </div>
+</div>
   <!-- organizer profile nav END -->
 
 
