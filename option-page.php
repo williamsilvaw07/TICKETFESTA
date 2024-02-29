84d873bb6118a6cb74a36190f3997fadd13b9c33
Arnab Mondal <armondal.am@gmail.com>
2024-03-01 00:30:16 +0600

    Update option-page.php

--- option-page.php
+++ option-page.php
@@ -85,15 +85,15 @@ $order = wc_get_order($order_id);
 // Get fees applied to the order
 $fees = $order->get_items(['fee']);
 echo '<pre>';
-var_dump($fees->get_total());
+var_dump($fees[0]);
 echo '</pre>';
 
 // Output the custom fees
 if (!empty($fees)) {
     echo "Custom Fees for Order ID $order_id:<br>";
-    foreach ($fees as $fee) {
-        echo "{$fee->name}: {$fee->amount}<br>";
-    }
+    // foreach ($fees as $fee) {
+    //     echo "{$fee->name}: {$fee->get_total()}<br>";
+    // }
 } else {
     echo "No custom fees found for order ID: $order_id";
 }
