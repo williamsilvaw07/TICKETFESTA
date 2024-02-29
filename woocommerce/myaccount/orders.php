721471d4b2b9d99c1d2f3b67b232268a0db5afd0
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-02-29 20:50:50 +0000

    Update orders.php

--- woocommerce/myaccount/orders.php
+++ woocommerce/myaccount/orders.php
@@ -247,7 +247,8 @@ tr.woocommerce-orders-table__row , thead tr{
 }
 
 .woocommerce table.shop_table th{
-	padding-right:0!important
+	padding-right:0!important;
+    align-items: center;
 }
 
 .oder_nm_link_main{
