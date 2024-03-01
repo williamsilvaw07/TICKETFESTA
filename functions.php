f3ce598d3c40d03b9b94231232378d09937280b8
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-01 14:12:14 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -3646,7 +3646,7 @@ function display_upcoming_events_for_user_with_view_order_button() {
                     ?>
                     <div class="ticketContainer">
                         <div class="ticket">
-                            <div class="ticketImage" style="background-image: url('<?php echo $event_image_url; ?>');"></div>
+                            <div class="ticketImage"<?php echo $event_image_url; ?>></div>
                             <div class="ticketTitle"><?php echo $event_title; ?></div>
                             <hr>
                             <div class="ticketDetail">
