54e14b896c7a482c707dc00034b49f80fadc3f11
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-01 09:28:39 +0000

    css

--- woocommerce/myaccount/navigation.php
+++ woocommerce/myaccount/navigation.php
@@ -37,3 +37,87 @@ $icons = [
 </nav>
 
 <?php do_action( 'woocommerce_after_account_navigation' ); ?>
+
+
+
+
+
+
+
+
+
+<style>
+
+
+
+/* Target the active link */
+.woocommerce-MyAccount-navigation-link.is-active a {
+    background: linear-gradient(270deg, rgba(211, 250, 22, 0.28) 0.01%, rgba(211, 250, 22, 0.00) 99.96%);
+    color: white;
+    position: relative;
+}
+
+.woocommerce-MyAccount-navigation .woocommerce-MyAccount-navigation-link.is-active a:after {
+    content: '';
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    width: 5px;
+    background-color: #d3fa16;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+    z-index: 1; /* Ensure the pseudo-element is above the link background */
+}
+
+
+
+
+
+
+
+
+
+
+
+@media (max-width: 700px) {
+	#custom-welcome-message{
+		display:none!important
+	}
+	.woocommerce-MyAccount-navigation{
+		width: 100%!important;
+    max-width: 100%;
+    height: 76px;
+    background-color: #000000;
+    position: fixed;
+    top: 91.1%;
+    left: 0px;
+    padding: 4px 4px;
+    padding-top: 0;
+    z-index: 99;
+	}
+	.woocommerce-MyAccount-navigation ul{
+	    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 0 4%;
+	}
+	.woocommerce-MyAccount-navigation li{
+		width: fit-content;
+	}
+
+.woocommerce-MyAccount-navigation .nav-label {
+    display: none; 
+}
+
+}
+
+
+
+
+
+
+
+
+	</style>
