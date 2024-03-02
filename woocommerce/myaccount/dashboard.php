07e48343d2f4a3dff24108b4174c37a4acb59115
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-02 17:48:45 +0000

    Update dashboard.php

--- woocommerce/myaccount/dashboard.php
+++ woocommerce/myaccount/dashboard.php
@@ -215,7 +215,7 @@ function display_upcoming_events_for_user_with_view_order_button() {
 }
 echo '</div>'; // Close the main container for all tickets
 
-add_action('woocommerce_account_dashboard', 'display_upcoming_events_for_user_with_view_order_button');
+
 
 
 
