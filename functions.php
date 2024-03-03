e89e15875fcb1e609eaf734e026889fe001771ed
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-03 15:47:44 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -3710,7 +3710,18 @@ function display_upcoming_events_for_user_with_view_order_button() {
 
                              <div class="ticket_inner_div ">
                              <div class="ticketTitle"><?php echo truncate_title($event_title, 30); ?></div>
-                            <div class="eventaddress"><?php echo $event_address; ?> <a class="opne_on_map_link" href="<?php echo $map_link; ?>" target="_blank">Map</a></div>
+                           <?php // Check if the event address is not empty
+if (!empty($event_address)) {
+    // Encode the address for URL use
+    $map_link = "https://maps.google.com/?q=" . urlencode($event_address);
+
+    // Display the address and the "Open on Map" button
+    echo '<div class="eventaddress">' . $event_address . ' <a class="open_on_map_link" href="' . $map_link . '" target="_blank">Open on Map</a></div>';
+} else {
+    // If there is no address, you can choose to not display anything, or customize as needed
+    echo '<div class="eventaddress">Address not available</div>'; // Optional: Customize based on your preference
+}
+?>
                             <hr>
                             <div class="ticketDetail">
     <div><span class="ticket-detail-title">Event Date:</span>&ensp;<?php echo date_i18n('F j, Y, g:i a', strtotime($event_start_date)); ?></div>
