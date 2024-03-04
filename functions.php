f73af701b546254d42886bcad1d07eedbb687c84
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-04 13:09:47 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -3648,7 +3648,7 @@ function ticketfeasta_display_following_organizers_events_dashboard()
 add_action('woocommerce_account_following_endpoint', 'ticketfeasta_display_following_organizers_events_dashboard');
 
 
-
+///
 
 
 
