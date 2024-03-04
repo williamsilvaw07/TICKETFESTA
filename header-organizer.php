5d0a8e7a468a7df31f238a71197798f5239c1097
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-04 14:28:49 +0000

    Update header-organizer.php

--- header-organizer.php
+++ header-organizer.php
@@ -8,8 +8,7 @@ if (!defined('ABSPATH')) {
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
     <!-- Include stylesheet -->
     <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
 
