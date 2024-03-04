097f4bd8c74ba29d4591d39897e3526f0f9f6eea
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-04 23:45:35 +0000

    w

---
+++ organizer-template.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Organizer Template
+*/
+
+get_header('organizer'); // Include the header
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+                      the_content();
+                endwhile;
+            endif;
+            ?>
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+get_footer('organizer'); // Include the footer
+
+
+
+
+
+
+
+
