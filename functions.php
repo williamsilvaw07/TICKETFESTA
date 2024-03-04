bdc97263dab477c6cbb7f66e3a03ea9104a36db4
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-04 19:01:12 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -3957,7 +3957,7 @@ function custom_user_profile_shortcode() {
 
     // Form HTML using Bootstrap layout
     $output = '
-    <div class="container">
+    <div class="container setting_page_admin">
         <h1>Edit Profile</h1>
         <form class="orgerinser_settings_form" method="post" id="adduser" action="' . get_permalink() . '">
             ' . wp_nonce_field('update_user_' . $user_id, '_wpnonce', true, false) . '
