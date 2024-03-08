a8085337ad9c5e38d910184c4a573747b4790652
Arnab Mondal <armondal.am@gmail.com>
2024-03-08 22:56:17 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,2 +1,7 @@
 <?php
 
+function my_enqueue_styles() {
+    wp_enqueue_style( 'font-awesome', 'https://cdn.jsdelivr.net/npm/fontawesome@4.7.0/css/font-awesome.min.css', array(), '4.7.0' ); // Adjust version number if needed
+  }
+  add_action( 'wp_enqueue_scripts', 'my_enqueue_styles' );
+  
