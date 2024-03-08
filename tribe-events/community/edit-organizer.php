dc77db7795df4a3c066f42ff8c160112dff8d808
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-08 00:45:41 +0000

    Update edit-organizer.php

--- tribe-events/community/edit-organizer.php
+++ tribe-events/community/edit-organizer.php
@@ -227,6 +227,7 @@ if ($banner_image_id) {
                 <?php printf( __( '%s Description:', 'tribe-events-community' ), $organizer_label_singular ); ?>
                 <small class="req"><?php esc_html_e( '(required)', 'tribe-events-community' ); ?></small>
             </label>
+
             <input type="textarea" placeholder="Organiser Description" name="organizer_description" id="organizer_description_input" value="<?php echo esc_attr( $organizer_description ); ?>" readonly/>
             <svg class="edit_svg_click organizer_description_edit_btn" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#d3fa16" viewBox="0 0 24 24"><path d="M4 16.668V20h3.333l9.83-9.83-3.333-3.332zm15.74-9.075a.885.885 0 0 0 0-1.253l-2.08-2.08a.885.885 0 0 0-1.253 0L14.78 5.886l3.333 3.333zM6 17l8-8 1 1-8 8z"></path></svg>
 
@@ -237,7 +238,8 @@ if ($banner_image_id) {
                 <?php printf( __( '%s Email:', 'tribe-events-community' ), $organizer_label_singular ); ?>
                 <small class="req"><?php esc_html_e( '(required)', 'tribe-events-community' ); ?></small>
             </label>
-            <input type="email" name="organizer_email" id="organizer_email" value="<?php echo esc_attr( $organizer_email ); ?>" readonly/>
+
+            <input type="email" placeholder="Organiser Email" name="organizer_email" id="organizer_email" value="<?php echo esc_attr( $organizer_email ); ?>" readonly/>
             <svg class="edit_svg_click organizer_email_edit_btn" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#d3fa16" viewBox="0 0 24 24"><path d="M4 16.668V20h3.333l9.83-9.83-3.333-3.332zm15.74-9.075a.885.885 0 0 0 0-1.253l-2.08-2.08a.885.885 0 0 0-1.253 0L14.78 5.886l3.333 3.333zM6 17l8-8 1 1-8 8z"></path></svg>
         </div>
         <div class="events-community-post-title facebook">
