0d6d27e14afc183b1355e8ba2943d2e8e5151087
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-11 16:08:57 +0000

    Update admin-custom-cat.js

--- js/admin-custom-cat.js
+++ js/admin-custom-cat.js
@@ -37,27 +37,3 @@ jQuery(document).ready(function($) {
 
 
 
-jQuery(document).ready(function($) { 
-    console.log( "ready!" );
-    function onScanSuccess(decodedText, decodedResult) {
-        // Here you can handle the decoded text, e.g., display it or send it to the server
-        console.log(decodedText, decodedResult);
-
-        // Sending scanned code to the server via AJAX
-        $.ajax({
-            type: 'POST',
-            url: ajax_object.ajax_url,
-            data: {
-                action: 'handle_qr_code_scan', // The AJAX handler action name
-                decodedText: decodedText // The scanned QR code text
-            },
-            success: function(response) {
-                alert(response.data.message); // Success response from server
-            }
-        });
-    }
-
-    // Initialize the QR code scanner
-    var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250 }, false);
-    html5QrcodeScanner.render(onScanSuccess);
-});
