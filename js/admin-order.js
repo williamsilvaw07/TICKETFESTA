242abeb387ebaca63d980c0af7bd19c1609db3a9
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-12 19:01:35 +0000

    S

--- js/admin-order.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($) {
-    let newText = '<strong>Total Site Fees:</strong> £' + window.order_data.site_fees;
-    jQuery('.tribe-event-meta-total-site-fees').html(newText);
-});
