41da95347fd99cbed8fdee8d63c19eeecb888845
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-16 14:28:52 +0000

    Update header-organizer.php

--- header-organizer.php
+++ header-organizer.php
@@ -3677,8 +3677,7 @@ display:block!important
                     padding-left: 5px;
                     font-size: 13px;
                     font-weight: 300;
-                    position: relative;
-    top: 1px;
+                    
 
                 }
 
@@ -3691,7 +3690,7 @@ display:block!important
 
                 .tribe-community-events-list tbody .tribe-list-column-organizer {
                     grid-area: 3 / 1 / 4 / 2;
-
+    line-height: 24px;
 }
                 }
 
