845a99ec0dd1298f3fc7f70403efc241490e7f26
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-19 19:19:35 +0000

    s

--- functions.php
+++ functions.php
@@ -4818,7 +4818,7 @@ function generate_unique_random_hash($length) {
 
 function my_enqueue_qrcode_script() {
     // Enqueue html5-qrcode script with jQuery dependency
-    wp_enqueue_script('html5-qrcode', 'https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.8/html5-qrcode.min.js', array('jquery'), null, true);
+    wp_enqueue_script('html5-qrcode', 'https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.7/html5-qrcode.min.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'my_enqueue_qrcode_script');
 
