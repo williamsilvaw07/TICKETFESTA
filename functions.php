c67a4bfc15ca5dada24c654f49d2e0367604eea7
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-20 12:59:04 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -4810,7 +4810,6 @@ function generate_unique_random_hash($length) {
 
 
 
-
 function my_enqueue_qrcode_script() {
     // Enqueue html5-qrcode script with jQuery dependency
     wp_enqueue_script('html5-qrcode', 'https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.7/html5-qrcode.min.js', array('jquery'), null, true);
@@ -4826,14 +4825,14 @@ function display_html5_qrcode_scanner_shortcode() {
 jQuery(document).ready(function($) {
     function onScanSuccess(decodedText, decodedResult) {
         // Handle the scanned text as needed.
-        console.log(\`Code scanned = \${decodedText}\`, decodedResult);
+        console.log(`Code scanned = ${decodedText}`, decodedResult);
     }
     
     var config = { fps: 10, qrbox: 250 };
     var html5QrCode = new Html5Qrcode("qr-reader");
     
-    // Check if camera access is already granted (by checking sessionStorage)
-    var cameraAccessGranted = sessionStorage.getItem('cameraAccess') === 'granted';
+    // Check if camera access is already granted (by checking localStorage)
+    var cameraAccessGranted = localStorage.getItem('cameraAccess') === 'granted';
     
     // If camera access is granted, start the QR code scanner immediately
     if (cameraAccessGranted) {
@@ -4848,8 +4847,8 @@ jQuery(document).ready(function($) {
         // If camera access is not granted, request camera access
         navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
         .then(function(stream) {
-            // Set sessionStorage to remember that camera access has been granted for this session
-            sessionStorage.setItem('cameraAccess', 'granted');
+            // Set localStorage to remember that camera access has been granted
+            localStorage.setItem('cameraAccess', 'granted');
             Html5Qrcode.getCameras().then(cameras => {
                 if (cameras.length > 0) {
                     html5QrCode.start(cameras[0].id, config, onScanSuccess); // Start QR code scanning
@@ -4867,6 +4866,6 @@ jQuery(document).ready(function($) {
 EOD;
 
     // Return the HTML for the scanner along with the inline JavaScript
-    return '<div id="qr-reader" style="max-width:400px; max-height:400px; width:100%; aspect-ratio: 1; position: relative; margin: 20px auto; overflow: hidden;"></div>' . $inline_script;
+    return '<div id="qr-reader" style="width:300px; height:300px;"></div>' . $inline_script;
 }
 add_shortcode('display_html5_qrcode_scanner', 'display_html5_qrcode_scanner_shortcode');
