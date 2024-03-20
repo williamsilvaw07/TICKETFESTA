b4fd4aca089401db998d69b35e0693725a7d6e45
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-20 15:31:09 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -4821,32 +4821,50 @@ function display_html5_qrcode_scanner_shortcode() {
     $scanner_html = '<div class="qrcode_scanner_wrapper_div"><div id="qr-reader" style="aspect-ratio: 1 / 1;"></div></div>';
 
     // Inline JavaScript for initializing the QR code scanner with a square viewfinder
-    $inline_script = "
+    $inline_script = <<<EOD
     <script>
     jQuery(document).ready(function($) {
-        let html5QrcodeScanner = new Html5QrcodeScanner(
-            'qr-reader', {
-                fps: 10,
-                qrbox: 250, // Set qrbox size to keep the scanning area square
-                rememberLastUsedCamera: true,
-                aspectRatio: 1,
-                disableFlip: true // Disable the option to flip camera
-            }, false);
-        
+        let html5QrcodeScanner;
+        let isScanning = false;
+
         function onScanSuccess(decodedText, decodedResult) {
             // Handle the scanned code as needed
             console.log(`Code scanned = ${decodedText}`, decodedResult);
         }
-        
-        html5QrcodeScanner.render(onScanSuccess);
+
+        function startScanning() {
+            if (!isScanning) {
+                isScanning = true;
+                Html5Qrcode.getCameras().then(cameras => {
+                    if (cameras.length > 0) {
+                        html5QrcodeScanner = new Html5QrcodeScanner(
+                            'qr-reader', {
+                                fps: 10,
+                                qrbox: 250, // Set qrbox size to keep the scanning area square
+                                aspectRatio: 1,
+                                disableFlip: true // Disable the option to flip camera
+                            }, false);
+                        html5QrcodeScanner.render(onScanSuccess);
+                    } else {
+                        console.error("No cameras found.");
+                    }
+                }).catch(err => {
+                    console.error("Unable to start QR scanner", err);
+                });
+            }
+        }
+
+        // Function to handle start scanning button click
+        $('#start-scanning-btn').click(function() {
+            startScanning();
+        });
     });
-    </script>";
+    </script>
+EOD;
 
     return $scanner_html . $inline_script;
 }
 add_shortcode('display_html5_qrcode_scanner', 'display_html5_qrcode_scanner_shortcode');
-
-
 
 
 
