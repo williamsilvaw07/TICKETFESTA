9fa088ec177e092ffff2ca97a7507a9de5c282bd
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-20 12:56:37 +0000

    admin@admin.com

--- functions.php
+++ functions.php
@@ -4807,6 +4807,11 @@ function generate_unique_random_hash($length) {
 
 
 
+
+
+
+
+
 function my_enqueue_qrcode_script() {
     // Enqueue html5-qrcode script with jQuery dependency
     wp_enqueue_script('html5-qrcode', 'https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.7/html5-qrcode.min.js', array('jquery'), null, true);
@@ -4863,6 +4868,6 @@ jQuery(document).ready(function($) {
 EOD;
 
     // Return the HTML for the scanner along with the inline JavaScript
-    return '<div id="qr-reader" style="width:300px; height:300px;"></div>' . $inline_script;
+    return '<div id="qr-reader" style="max-width:400px; max-height:400px; width:100%; aspect-ratio: 1; position: relative; margin: 20px auto; overflow: hidden;"></div>' . $inline_script;
 }
 add_shortcode('display_html5_qrcode_scanner', 'display_html5_qrcode_scanner_shortcode');
