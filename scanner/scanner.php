274a59b1535bbb7fcc1fe684149988da61e87793
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-21 20:40:34 +0000

    Update scanner.php

--- scanner/scanner.php
+++ scanner/scanner.php
@@ -152,18 +152,12 @@ body:not(.sidebar-mini-md):not(.sidebar-mini-xs):not(.layout-top-nav) .content-w
     width: 100%;
 }
 .scanner_login_div{
-    background-color: #19191b;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    position: relative;
-    padding: 17px;
-    border-radius: 10px;
-    width: 100%;
+
 
 }
 .scanner_login_div p{
     font-weight: 300;
+    color: #aaa;
 }
 
 
