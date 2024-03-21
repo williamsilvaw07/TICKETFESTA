e5dc79d8c29e60ab2145916781953e6352f16a1e
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-21 18:50:27 +0000

    Update header-organizer-scanner.php

--- scanner/header-organizer-scanner.php
+++ scanner/header-organizer-scanner.php
@@ -34,7 +34,7 @@ if (!defined('ABSPATH')) {
 <!-- Brand Logo -->
 <a href="https://ticketfesta.co.uk/" class="brand-link">
                 <img src="https://ticketfesta.co.uk/wp-content/uploads/2024/02/Group-195-2.png" alt=""
-                    class="brand-image img-circle elevation-3">
+                    class="brand-image  elevation-3">
                 <span class="brand-text font-weight-light"></span>
             </a>
             
