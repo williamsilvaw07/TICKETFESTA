06f93cea0a7a52931553e5ceb0757fb3e3f06fa6
Arnab Mondal <armondal.am@gmail.com>
2024-03-22 22:01:15 +0600

    Update event-list.php

--- tribe-events/community/event-list.php
+++ tribe-events/community/event-list.php
@@ -223,7 +223,7 @@ do_action( 'tribe_community_events_before_list_table' );
                         ] );
                     }
                     if ( 'status' === $column_slug ) {
-                        echo "</td><td>$column_slug";
+                        echo "</td><td><span class='passcode'>$column_slug</span>";
                     }
                     ?>
                 </td>
