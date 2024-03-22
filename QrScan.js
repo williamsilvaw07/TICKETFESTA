f8a57f0db6c696c34cbf46892343c68e367bfed5
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-22 23:23:21 +0000

    Update QrScan.js

--- QrScan.js
+++ QrScan.js
@@ -317,10 +317,12 @@
                 // HTML for individual progress components with the same class names as before
                 var individualProgressHtml = `
                     <div class="ticket-progress-container">
+                    <div class="ticket-progress-container_svg">
                         <svg class="progress-ring" width="72" height="72">
                             <circle class="progress-ring__circle-bg" cx="36" cy="36" r="31" stroke-width="6"></circle>
                             <circle class="progress-ring__circle progress-ring__circle-individual" cx="36" cy="36" r="31" stroke-width="6"></circle>
                         </svg>
+                        </div>
                         <span class="progress-percentage_individual">${percentage}%</span>
                         <div class="ticket-details">
                             <div class="ticket-name">${ticket.name}</div>
