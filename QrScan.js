806201d5db8592ef537342b4fe889665f31b84ec
Arnab Mondal <armondal.am@gmail.com>
2024-03-24 00:21:42 +0600

    Update QrScan.js

--- QrScan.js
+++ QrScan.js
@@ -415,7 +415,7 @@
          CheckProgressData();
          
          // Set an interval to call the function every 30 seconds
-         var intervalId = setInterval(CheckProgressData, 10000);
+         var intervalId = setInterval(CheckProgressData, 30000);
          
          // (Optional) Clear the interval when the user leaves the page
         //  $(window).unload(function() {
