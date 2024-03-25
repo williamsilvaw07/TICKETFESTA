f58dc0bcefb89004f1b9646a81c71c7a808b459e
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-25 23:07:39 +0000

    Update scanner.php

--- scanner/scanner.php
+++ scanner/scanner.php
@@ -255,7 +255,7 @@ input#event-pass {
         display: flex;
     flex-direction: column !important;
     margin: 0 auto;
-
+    padding: 0 15px !important;
     position: relative;
     border-radius: 10px;
     width: 100%;
