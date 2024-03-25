8ae8dc4f7f8bc23cb7a90ef6af4640764632798d
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-25 16:45:26 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -4573,8 +4573,6 @@ function custom_qr_scanner_shortcode() {
 
 <div class="ticket_checkedin_main_stats main_stats_block event-container">
     <i class="fas fa-check-circle"></i>
-    <h6>Checked-in Tickets</h6>
-
     <div class="checkedin_ticket-info info_div">
         <h6>Checked-in Tickets</h6>
         <p class="stats_count_main checkedin_ticket-count">0<span>/</span>0</p>
