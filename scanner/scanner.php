32cc02d0db1026d5101c6ecf1454f94bccca0ede
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-03-25 23:46:05 +0000

    Update scanner.php

--- scanner/scanner.php
+++ scanner/scanner.php
@@ -58,10 +58,9 @@ if (file_exists($custom_footer_path)) {
 
 
 <style>
-   html .main-header ul:first-child{
-        display:none!important
-    }
-
+   html.main-header nav ul:first-of-type {
+    display: none!important;
+}
 body:not(.sidebar-mini-md):not(.sidebar-mini-xs):not(.layout-top-nav) .content-wrapper, body:not(.sidebar-mini-md):not(.sidebar-mini-xs):not(.layout-top-nav) .main-footer, body:not(.sidebar-mini-md):not(.sidebar-mini-xs):not(.layout-top-nav) .main-header {
 
     margin-left: inherit;
