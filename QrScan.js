c1017209d014802b4b8ec6eb4e8461d023b30cd4
Arnab Mondal <armondal.am@gmail.com>
2024-03-26 18:57:09 +0600

    Update QrScan.js

--- QrScan.js
+++ QrScan.js
@@ -512,7 +512,7 @@ function passcodeMatch(response) {
          CheckProgressData();
          
          // Set an interval to call the function every 30 seconds
-         var intervalId = setInterval(CheckProgressData, 300000);
+         var intervalId = setInterval(CheckProgressData, 30000);
          
          // (Optional) Clear the interval when the user leaves the page
         //  $(window).unload(function() {
