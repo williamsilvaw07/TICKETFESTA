0f652bf5c3202f7cd5d41adcb3704ca878bb2058
Arnab Mondal <armondal.am@gmail.com>
2024-03-28 23:55:34 +0600

    Update event-dashboard-ajax.php

--- event-dashboard-ajax.php
+++ event-dashboard-ajax.php
@@ -7,9 +7,11 @@ add_action('wp_ajax_nopriv_add_event_attendee', 'add_event_attendee');
 function add_event_attendee(){
 
     $event_id = isset($_POST['event_id']) ?  esc_attr($_POST['event_id']) : null;
-    var_dump(get_post_meta( $event_id));
+    // var_dump(get_post_meta( $event_id));
     $result = [
-        'event_id' => $event_id
+        'event_id' => $event_id,
+        'data' => get_post_meta( $event_id),
     ];
-    wp_send_json_success( $result);
+    var_dump( $result);
+    die();
 }
