70107862780a636a5949512975e937221ff2998d
Arnab Mondal <armondal.am@gmail.com>
2024-03-29 00:01:16 +0600

    Update event-custom-features.js

--- event-custom-features.js
+++ event-custom-features.js
@@ -9,6 +9,7 @@
     function request_add_attendee(){
         let eventID = $(this).data('event-id');
         $.ajax({
+            url: window.tribe_ajax.ajax_url,
             type: 'POST',
             data: {
                 action: 'custom_add_event_attendee',
