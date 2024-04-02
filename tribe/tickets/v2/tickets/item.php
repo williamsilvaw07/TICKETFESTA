3426a5b7d5b58dddb907e1d16f73d8d5ab2fb083
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-04-02 11:38:17 +0100

    Update item.php

--- tribe/tickets/v2/tickets/item.php
+++ tribe/tickets/v2/tickets/item.php
@@ -11,43 +11,43 @@
  * @link    https://evnt.is/1amp Help article for RSVP & Ticket template files.
  *
  * @since 5.0.3
- * @since 5.1.1 Display data attributes via `tribe_attributes` and make them filterable via `tribe_tickets_block_ticket_html_attributes`.
- * @since 5.1.6 Add the `data-available-count` attribute for each ticket to calculate the shared capacity availability correctly.
- * @since 5.1.9 Add the `data-ticket-price` attribute for each ticket to calculate the price precisely.
- * @since 5.1.10 Cast the `data-ticket-price` attribute value as string to avoid unwanted PHP errors.
- * @since 5.5.6 Add the `tribe-tickets__tickets-item--shared-capacity` wrapper class for tickets having shared capacity.
+ * @since 5.1.1 Display data attributes via tribe_attributes and make them filterable via tribe_tickets_block_ticket_html_attributes.
+ * @since 5.1.6 Add the data-available-count attribute for each ticket to calculate the shared capacity availability correctly.
+ * @since 5.1.9 Add the data-ticket-price attribute for each ticket to calculate the price precisely.
+ * @since 5.1.10 Cast the data-ticket-price attribute value as string to avoid unwanted PHP errors.
+ * @since 5.5.6 Add the tribe-tickets__tickets-item--shared-capacity wrapper class for tickets having shared capacity.
  *
  * @version 5.5.6
  *
  * If RSVP:
- * @var Tribe__Tickets__Editor__Template   $this                        [Global] Template object.
+ * @var Tribe_TicketsEditor_Template   $this                        [Global] Template object.
  * @var null|bool                          $is_modal                    [Global] Whether the modal is enabled.
  * @var int                                $post_id                     [Global] The current Post ID to which tickets are attached.
- * @var Tribe__Tickets__Tickets            $provider                    [Global] The tickets provider class.
+ * @var Tribe_Tickets_Tickets            $provider                    [Global] The tickets provider class.
  * @var string                             $provider_id                 [Global] The tickets provider class name.
  * @var string                             $cart_url                    [Global] Link to Cart (could be empty).
- * @var Tribe__Tickets__Ticket_Object[]    $tickets                     [Global] List of tickets.
- * @var Tribe__Tickets__Ticket_Object[]    $tickets_on_sale             [Global] List of tickets on sale.
+ * @var Tribe_Tickets_Ticket_Object[]    $tickets                     [Global] List of tickets.
+ * @var Tribe_Tickets_Ticket_Object[]    $tickets_on_sale             [Global] List of tickets on sale.
  * @var bool                               $has_tickets_on_sale         [Global] True if the event has any tickets on sale.
  * @var bool                               $is_sale_past                [Global] True if tickets' sale dates are all in the past.
  * @var bool                               $is_sale_future              [Global] True if no ticket sale dates have started yet.
- * @var Tribe__Tickets__Commerce__Currency $currency                    [Global] Tribe Currency object.
- * @var Tribe__Tickets__Ticket_Object      $ticket                      The ticket object.
+ * @var Tribe_TicketsCommerce_Currency $currency                    [Global] Tribe Currency object.
+ * @var Tribe_Tickets_Ticket_Object      $ticket                      The ticket object.
  *
  * If Ticket:
- * @var Tribe__Tickets__Editor__Template   $this                        [Global] Template object.
+ * @var Tribe_TicketsEditor_Template   $this                        [Global] Template object.
  * @var int                                $post_id                     [Global] The current Post ID to which tickets are attached.
- * @var Tribe__Tickets__Tickets            $provider                    [Global] The tickets provider class.
+ * @var Tribe_Tickets_Tickets            $provider                    [Global] The tickets provider class.
  * @var string                             $provider_id                 [Global] The tickets provider class name.
- * @var Tribe__Tickets__Ticket_Object[]    $tickets                     [Global] List of tickets.
+ * @var Tribe_Tickets_Ticket_Object[]    $tickets                     [Global] List of tickets.
  * @var array                              $cart_classes                [Global] CSS classes.
- * @var Tribe__Tickets__Ticket_Object[]    $tickets_on_sale             [Global] List of tickets on sale.
+ * @var Tribe_Tickets_Ticket_Object[]    $tickets_on_sale             [Global] List of tickets on sale.
  * @var bool                               $has_tickets_on_sale         [Global] True if the event has any tickets on sale.
  * @var bool                               $is_sale_past                [Global] True if tickets' sale dates are all in the past.
  * @var bool                               $is_sale_future              [Global] True if no ticket sale dates have started yet.
- * @var Tribe__Tickets__Commerce__Currency $currency                    [Global] Tribe Currency object.
- * @var Tribe__Tickets__Tickets_Handler    $handler                     [Global] Tribe Tickets Handler object.
- * @var Tribe__Tickets__Privacy            $privacy                     [Global] Tribe Tickets Privacy object.
+ * @var Tribe_TicketsCommerce_Currency $currency                    [Global] Tribe Currency object.
+ * @var Tribe_Tickets_Tickets_Handler    $handler                     [Global] Tribe Tickets Handler object.
+ * @var Tribe_Tickets_Privacy            $privacy                     [Global] Tribe Tickets Privacy object.
  * @var int                                $threshold                   [Global] The count at which "number of tickets left" message appears.
  * @var bool                               $show_original_price_on_sale [Global] Show original price on sale.
  * @var null|bool                          $is_mini                     [Global] If in "mini cart" context.
@@ -55,7 +55,7 @@
  * @var string                             $submit_button_name          [Global] The button name for the tickets block.
  * @var string                             $cart_url                    [Global] Link to Cart (could be empty).
  * @var string                             $checkout_url                [Global] Link to Checkout (could be empty).
- * @var Tribe__Tickets__Ticket_Object      $ticket                      The ticket object.
+ * @var Tribe_Tickets_Ticket_Object      $ticket                      The ticket object.
  * @var int                                $key                         Ticket Item index.
  * @var string                             $data_available              Boolean string.
  * @var bool                               $has_shared_cap              True if ticket has shared capacity.
@@ -74,10 +74,10 @@ if (
 	return false;
 }
 
-/* @var Tribe__Tickets__Privacy $privacy */
+/* @var Tribe_Tickets_Privacy $privacy */
 $privacy = tribe( 'tickets.privacy' );
 
-/** @var Tribe__Tickets__Tickets_Handler $tickets_handler */
+/** @var Tribe_Tickets_Tickets_Handler $tickets_handler */
 $handler = tribe( 'tickets.handler' );
 
 $context = [
@@ -122,7 +122,7 @@ if ( $has_shared_cap ) {
  * @since 5.1.1
  *
  * @param array $attributes A list of data attributes with their values.
- * @param Tribe__Tickets__Ticket_Object $ticket The ticket object.
+ * @param Tribe_Tickets_Ticket_Object $ticket The ticket object.
  */
 $attributes = apply_filters( 'tribe_tickets_block_ticket_html_attributes', $attributes, $ticket );
 ?>
@@ -142,8 +142,8 @@ $attributes = apply_filters( 'tribe_tickets_block_ticket_html_attributes', $attr
 	<?php $this->template( 'v2/tickets/item/quantity', $context ); ?>
 
 	<?php $this->template( 'v2/tickets/item/quantity-mini', $context ); ?>
-<?php	}?>
+
 
 	<?php $this->template( 'v2/tickets/item/opt-out', $context ); ?>
-
+<?php	}?>
 </div>
