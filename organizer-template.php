ad08bd58c4732463e6b8479d95a07fab13ba37ee
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-04-06 18:47:30 +0100

    Update organizer-template.php

--- organizer-template.php
+++ organizer-template.php
@@ -45,8 +45,6 @@ get_header();
 <!-- /.content-wrapper -->
 
 <script>
-
-
 function switchUserRole() {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/wp-admin/admin-ajax.php", true);
@@ -60,29 +58,7 @@ function switchUserRole() {
     xhr.send("action=switch_to_organiser");
 }
 
-
-
 </script>
-<!-- Bootstrap Modal for Notifications -->
-<div class="modal fade" id="roleChangeModal" tabindex="-1" role="dialog" aria-labelledby="roleChangeModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="roleChangeModalLabel">Update Successful</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        Your account has been updated to organiser.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
 
 <?php
 // Include the custom footer
