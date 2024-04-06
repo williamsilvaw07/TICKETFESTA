da194a99584be7bf2c52d58e73e6130e80d1a165
William Rodrigues da Silva <williamsilvaw07@gmail.com>
2024-04-06 18:23:01 +0100

    Update organizer-template.php

--- organizer-template.php
+++ organizer-template.php
@@ -1,24 +1,32 @@
 <?php
 /*
-Template Name: Organizer Template
+Template Name: Custom Access Control Template
 */
 
-get_header('organizer'); // Include the header
+// Include the custom header
+get_header();
+
 ?>
 
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-
     <!-- Main content -->
     <div class="content">
         <div class="container-fluid">
             <?php
-            if (have_posts()) :
-                while (have_posts()) :
-                    the_post();
-                      the_content();
-                endwhile;
-            endif;
+            // Check if the user is logged in and has the 'organiser' or 'administrator' role
+            if (is_user_logged_in() && (current_user_can('organiser') || current_user_can('administrator'))) {
+                // User is logged in and has the required role, display the content
+                if (have_posts()) :
+                    while (have_posts()) :
+                        the_post();
+                        the_content(); // The main content of the page
+                    endwhile;
+                endif;
+            } else {
+                // User is not logged in or does not have the required role, show login form
+                echo do_shortcode('[xoo_el_inline_form tabs="login" active="login"]');
+            }
             ?>
         </div><!-- /.container-fluid -->
     </div>
@@ -27,8 +35,9 @@ get_header('organizer'); // Include the header
 <!-- /.content-wrapper -->
 
 <?php
-get_footer('organizer'); // Include the footer
-
+// Include the custom footer
+get_footer();
+?>
 
 
 
