4f27160079b8332c3c8a0d68e59f1c8d925c2dd5
Sabuj Das <sabujdas9@gmail.com>
2024-04-09 00:58:04 +0600

    aadd add_custom_script_to_footer

--- coupon_auto_apply.php
+++ coupon_auto_apply.php
@@ -1,7 +1,8 @@
 <?php
 
 add_action('init', 'start_session', 1);
-function start_session() {
+function start_session()
+{
     if (!session_id()) {
         session_start();
     }
@@ -9,21 +10,38 @@ function start_session() {
     if (isset($_GET['coupon'])) {
         $_SESSION['applied_coupon'] = sanitize_text_field($_GET['coupon']);
     }
-    
+
 }
 
 // Function to set session variable if coupon parameter is present
-function set_coupon_session() {
+function set_coupon_session()
+{
     if (isset($_GET['coupon'])) {
         $_SESSION['applied_coupon'] = sanitize_text_field($_GET['coupon']);
     }
 }
 
+function add_custom_script_to_footer()
+{
+    if (isset($_GET['coupon'])) {
+        // Your JavaScript code here
+        $custom_script = "
+        // Your JavaScript code goes here
+        console.log('This script runs in the footer.');
+    ";
+        // Add the JavaScript code to the footer
+        wp_add_inline_script('jquery', $custom_script);
+    }
+}
+add_action('wp_enqueue_scripts', 'add_custom_script_to_footer');
+
+
 // Hook the function to set session variable when the WooCommerce product page loads
 //add_action('woocommerce_before_single_product', 'set_coupon_session');
 
 // Function to display the price drop
-function display_price_with_discount() {
+function display_price_with_discount()
+{
     global $product;
 
     // Check if coupon session variable is set
@@ -47,7 +65,8 @@ add_action('woocommerce_single_product_summary', 'display_price_with_discount', 
 
 
 // Function to automatically apply coupon when product is added to cart
-function auto_apply_coupon_to_cart($cart) {
+function auto_apply_coupon_to_cart($cart)
+{
     if (is_admin() && !defined('DOING_AJAX')) {
         return;
     }
@@ -55,12 +74,12 @@ function auto_apply_coupon_to_cart($cart) {
     if (empty($cart->get_applied_coupons())) {
         if (isset($_SESSION['applied_coupon'])) {
             $coupon_code = $_SESSION['applied_coupon'];
-            
+
             $coupon = new WC_Coupon($coupon_code);
-            
+
             if ($coupon->is_valid()) {
                 $cart->apply_coupon($coupon_code);
-                
+
                 $_SESSION['applied_coupon'] = null;
             }
         }
